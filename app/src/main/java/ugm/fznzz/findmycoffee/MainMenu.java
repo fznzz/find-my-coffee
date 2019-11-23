af43f0db6d97134fe416025fc4c7274d473e1f44
fznzz <39457927+fznzz@users.noreply.github.com>
2019-11-23 19:53:43 +0700

    Initial commit

--- app/src/main/java/ugm/fznzz/findmycoffee/MainMenu.java
+++ app/src/main/java/ugm/fznzz/findmycoffee/MainMenu.java
@@ -33,9 +33,6 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
-
-
     }
 
 
